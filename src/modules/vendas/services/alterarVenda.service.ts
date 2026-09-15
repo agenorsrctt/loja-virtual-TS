53819df25dafc674bc47af55ffbe57df7a8d4670
Agenor <agenorsrctt@gmail.com>
2026-09-15 12:14:20 -0300

    feat: adicionar alterarVenda.service

---
+++ src/modules/vendas/services/alterarVenda.service.ts
@@ -0,0 +1,39 @@
+import type { AlterarVendaDTO } from "../dtos/venda.dto.js";
+
+import { alterarVendaRepository } from "../repositories/alterarVenda.repository.js";
+
+import { validarIdVenda, validarDadosVenda, validarItensVenda } from "../utils/validarVenda.util.js";
+
+import { ErroVenda } from "../utils/erroVenda.util.js";
+
+export async function alterarVendaService(dados: AlterarVendaDTO, empresa_id: number, usuario_id: number, id: number) {
+
+    validarIdVenda(empresa_id, "Empresa");
+
+    validarIdVenda(usuario_id, "Usuário");
+
+    validarIdVenda(id, "Venda");
+
+    validarDadosVenda(dados);
+
+    if (dados.cliente_id === undefined && dados.itens === undefined) {
+
+        throw new ErroVenda("Informe ao menos um campo para alterar.", 400);
+
+    }
+
+    if (dados.cliente_id !== undefined) {
+
+        validarIdVenda(dados.cliente_id, "Cliente");
+
+    }
+
+    if (dados.itens !== undefined) {
+
+        validarItensVenda(dados.itens);
+
+    }
+
+    return await alterarVendaRepository(dados, empresa_id, usuario_id, id);
+
+}
