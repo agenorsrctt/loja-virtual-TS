fcf94217b5c854033fa213eb715c2b7711ca15a0
Agenor <agenorsrctt@gmail.com>
2026-09-15 17:04:30 -0300

    feat: integrar frontend em administracao.routes.ts

--- backend/src/modules/acesso/routes/administracao.routes.ts
+++ backend/src/modules/acesso/routes/administracao.routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 
+import { perfilSuperAdminController } from "../controllers/perfil.controller.js";
+
 import { loginSuperAdminController, alterarSenhaSuperAdminController } from "../controllers/acesso.controller.js";
 
 import { autenticarSuperAdmin } from "../../middleware/autenticacao.middleware.js";
 
 const rotasAdministracao = express.Router();
+
+rotasAdministracao.get("/perfil", autenticarSuperAdmin, perfilSuperAdminController);
 
 rotasAdministracao.post("/login", loginSuperAdminController);
 
