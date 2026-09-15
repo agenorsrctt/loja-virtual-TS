89063dfa884279711621f343c8d80ade8ee3caf5
Agenor <agenorsrctt@gmail.com>
2026-09-15 16:25:21 -0300

    feat: implementar acesso em buscarEmailUsuario.repository.ts

--- src/modules/usuarios/repositories/buscarEmailUsuario.repository.ts
+++ src/modules/usuarios/repositories/buscarEmailUsuario.repository.ts
@@ -1,18 +1,25 @@
 import { type UsuarioDto } from "../dtos/interfacesUsuario.js";
+
 import db from "../../../database/connection.js";
 
 
 export function buscarEmailUsuarioRepository(email: string, empresa_id: number): Promise<UsuarioDto | undefined>{
 
-    const sql = "SELECT * FROM USUARIOS WHERE email = ? AND empresa_id";
+    const sql = "SELECT * FROM USUARIOS WHERE email = ? AND empresa_id = ?";
 
     return new Promise<UsuarioDto>((resolve, reject) => {
-        db.get<UsuarioDto>(sql, email, (erro, usuario) => {
+
+        db.get<UsuarioDto>(sql, [email, empresa_id], (erro, usuario) => {
+
             if(erro) {
                 return reject(new Error("Erro do banco: " + erro));
+
             };
 
             resolve(usuario);
+
         });
+
     });
+
 };
