214debb294a9334c4a1e96fa8dcac573817ceb9b
Agenor <agenorsrctt@gmail.com>
2026-09-15 12:10:18 -0300

    feat: adicionar venda.dto

---
+++ src/modules/vendas/dtos/venda.dto.ts
@@ -0,0 +1,43 @@
+import type { CriarItemVendidoDTO, ItemVendidoDTO } from "../../itens_vendidos/dtos/itemVendido.dto.js";
+
+export type StatusVenda = "concluida" | "cancelada";
+
+export interface CriarVendaDTO {
+
+    cliente_id: number;
+
+    itens: CriarItemVendidoDTO[];
+
+}
+
+export interface AlterarVendaDTO {
+
+    cliente_id?: number;
+
+    itens?: CriarItemVendidoDTO[];
+
+}
+
+export interface VendaDTO {
+
+    readonly id: number;
+
+    readonly empresa_id: number;
+
+    readonly usuario_id: number;
+
+    cliente_id: number;
+
+    readonly data: string;
+
+    valor_total: number;
+
+    status: StatusVenda;
+
+}
+
+export interface VendaDetalhadaDTO extends VendaDTO {
+
+    itens: ItemVendidoDTO[];
+
+}
