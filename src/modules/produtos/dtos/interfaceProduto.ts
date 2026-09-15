42d78bf9d711013694dec647d07fb578cee49a71
Agenor <agenorsrctt@gmail.com>
2026-09-14 22:19:39 -0300

    feat: adicionado dtos de criar e alterar Produtos

--- src/modules/produtos/dtos/interfaceProduto.ts
+++ src/modules/produtos/dtos/interfaceProduto.ts
@@ -10,3 +10,26 @@ export interface ProdutosDTO {
     codigo: string;
     status: StatusProdutos
 }
+
+export interface CriarProdutoDTO {
+    readonly id: number;
+    readonly empresa_id: number;
+    produto: string;
+    estoque: number;
+    preco: number;
+    categoria?: string;
+    codigo?: string;
+    status: StatusProdutos
+}
+
+
+export interface AlterarProdutoDTO {
+    readonly id: number;
+    readonly empresa_id: number;
+    produto?: string;
+    estoque?: number;
+    preco?: number;
+    categoria?: string;
+    codigo?: string;
+    status?: StatusProdutos
+}
