1791bb1e4260f996ba07c2f50438b108f69ceedd
Agenor <agenorsrctt@gmail.com>
2026-09-15 16:25:21 -0300

    feat: implementar acesso em alterarUsuario.repository.ts

--- src/modules/usuarios/repositories/alterarUsuario.repository.ts
+++ src/modules/usuarios/repositories/alterarUsuario.repository.ts
@@ -1,53 +1,73 @@
 import db from "../../../database/connection.js";
+
 import type { AlterarUsuarioDto } from "../dtos/interfacesUsuario.js";
 
 export function alterarUsuarioRepository(dados: AlterarUsuarioDto, empresa_id: number, id: number): Promise<void> {
 
     const valores: (string | number)[] = [];
+
     const campos: string[] = [];
 
     if (dados.nome) {
         valores.push(dados.nome);
+
         campos.push("nome = ?");
+
     }
 
     if (dados.tipo) {
         valores.push(dados.tipo);
+
         campos.push("tipo = ?");
+
     }
 
     if (dados.email) {
         valores.push(dados.email);
+
         campos.push("email = ?");
+
     }
 
     if (dados.status) {
         valores.push(dados.status);
+
         campos.push("status = ?");
+
     }
 
     if (dados.senha) {
         valores.push(dados.senha);
+
         campos.push("senha = ?");
+
     }
 
     if (campos.length === 0) {
         throw new Error("Nenhuma alteração contabilizada.");
+
     }
+
+    campos.push("versao_token = versao_token + 1");
 
     const sql = `UPDATE USUARIOS SET ${campos.join(", ")} WHERE empresa_id = ? AND id = ?`;
 
     return new Promise<void>((resolve, reject) => {
+
         db.run(sql, [...valores, empresa_id, id], function (erro) {
+
             if (erro) {
                 return reject(new Error("AlterarRepository Error: " + erro));
+
             }
 
             if (this.changes === 0) {
                 return reject(new Error("AlterarRepository Error: Usuario não localizado."));
+
             }
 
             resolve();
+
         })
     })
 }
