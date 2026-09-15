d53e4844d309bed8bc5dd2d1cedcd6403fd9b488
Agenor <agenorsrctt@gmail.com>
2026-09-14 22:25:12 -0300

    feat: adicionado criarProdutoController

---
+++ src/modules/produtos/controllers/criarProduto.controller.ts
@@ -0,0 +1,37 @@
+import type { Request, Response } from "express";
+import type { CriarProdutoDTO } from "../dtos/interfaceProduto.js";
+
+
+
+export async function criarProdutoController(req: Request, res: Response) {
+
+    try {
+
+        const empresa_id = res.locals.usuario.empresa_id;
+
+        const produto: CriarProdutoDTO = req.body;
+
+        await criarProdutoService(produto, empresa_id);
+
+        res.status(201).json({
+            mensagem: "Produto criado com sucesso!"
+        })
+
+
+    } catch (error) {
+
+        if (error instanceof Error) {
+            return res.status(500).json({
+                mensagem: "Erro do servidor: " + error.message
+            })
+        }
+
+        return res.status(500).json({
+            mensagem: "Erro do servidor: "
+        })
+
+
+    }
+
+
+}
