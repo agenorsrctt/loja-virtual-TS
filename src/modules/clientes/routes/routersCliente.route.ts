34cd6a7d749ad4fb1806e137f1675ec4080efb02
Agenor <agenorsrctt@gmail.com>
2026-09-14 22:08:41 -0300

    feat: adicionado rotas do cliente

--- src/modules/clientes/routes/routersCliente.route.ts
+++ src/modules/clientes/routes/routersCliente.route.ts
@@ -1,8 +1,19 @@
 import express from 'express';
 import { criarClienteController } from '../controllers/criarCliente.controller.js';
+import { listarClienteController } from '../controllers/listaCliente.controller.js';
+import { buscarClienteController } from '../controllers/buscarCliente.controller.js';
+import { alterarClienteController } from '../controllers/alterarCliente.controller.js';
+import { inativarClienteController } from '../controllers/inativarCliente.controller.js';
+import { autenticar } from '../../middleware/jwt.js';
 
 const clienteRouter = express.Router();
 
-clienteRouter.post("/", criarClienteController);
+clienteRouter.get("/", autenticar ,listarClienteController);
+clienteRouter.get("/:id", autenticar , buscarClienteController);
+
+clienteRouter.post("/", autenticar , criarClienteController);
+
+clienteRouter.patch("/:id", autenticar , alterarClienteController);
+clienteRouter.delete("/:id", autenticar , inativarClienteController);
 
 export default clienteRouter;
