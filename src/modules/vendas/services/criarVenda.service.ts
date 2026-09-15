60b545c00860ccde5cac9ceee81fca16c910d0bd
Agenor <agenorsrctt@gmail.com>
2026-09-15 12:14:20 -0300

    feat: adicionar criarVenda.service

---
+++ src/modules/vendas/services/criarVenda.service.ts
@@ -0,0 +1,21 @@
+import type { CriarVendaDTO } from "../dtos/venda.dto.js";
+
+import { criarVendaRepository } from "../repositories/criarVenda.repository.js";
+
+import { validarIdVenda, validarDadosVenda, validarItensVenda } from "../utils/validarVenda.util.js";
+
+export async function criarVendaService(dados: CriarVendaDTO, empresa_id: number, usuario_id: number) {
+
+    validarIdVenda(empresa_id, "Empresa");
+
+    validarIdVenda(usuario_id, "Usuário");
+
+    validarDadosVenda(dados);
+
+    validarIdVenda(dados.cliente_id, "Cliente");
+
+    validarItensVenda(dados.itens);
+
+    return await criarVendaRepository(dados, empresa_id, usuario_id);
+
+}
